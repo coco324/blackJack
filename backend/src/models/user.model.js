0af921f7e21e1edba1a46a7ad3513959928f1fdc
coco <corantin.antony@gmail.com>
2025-12-11 15:30:16 +0100

    backend function create user, login, logout

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,10 @@
+class User {
+  constructor({ mail, password, username }) {
+    this.mail = mail;
+    this.password = password;
+    this.username = username;
+    this.solde = 0;
+  }
+}
+
+export default User;
